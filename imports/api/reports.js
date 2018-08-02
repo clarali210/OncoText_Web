e53858add2193eb29638661226a6d33f9a086a05
clarali210 <clarali2000@126.com>
2018-08-02 10:19:08 -0400

    working

--- imports/api/reports.js
+++ imports/api/reports.js
@@ -47,7 +47,7 @@ if (Meteor.isServer) {
       // for (var ind in keyValues){
       //   reports = reports.concat(Reports[organ].find({[key]: keyValues[ind]}).fetch());
       // }
-      var reports = Reports[organ].find({[key]: { $in: keyValues}).fetch()
+      var reports = Reports[organ].find({[key]: { $in: keyValues}}).fetch();
       reports = reports.filter((n) => { return n != undefined });
       return reports;
     }
